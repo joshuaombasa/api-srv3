710c14b35ce416e08d86a61ff3d2000229a5620d
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-07 01:47:40 +0300

    initial commit

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,55 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
+import express from 'express';
+require('express-async-errors');
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
+import { signupRouter } from './routes/signup';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { currentUserRouter } from './routes/current-user';
 
-import { signupRouter } from './routes/signup'
+import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
-import { errorHandler } from './middleware/error-handler'
+const PORT = 3000;
+const app = express();
 
-const PORT =3000
-const app = express()
+app.set('trust proxy', true);
 
-app.set('trust proxy', true)
+app.use(express.json());
 
-app.use(express.json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
 
-app.use(cookieSession({
-  signed: false,
-  secure: false
-}))
+app.use(signupRouter);
 
-app.use(signupRouter)
-app.use(errorHandler)
+app.use(signinRouter);
 
+app.use(signoutRouter);
 
+app.use(currentUserRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 const start = () => {
-  mongoose.connect('mongodb://127.0.0.1:27017/tiketi3-auth?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1')
-  .then(() => console.log('connected to mongodb'))
-  .catch(error => console.log(error))
+  mongoose
+    .connect(
+      'mongodb://127.0.0.1:27017/tiketi3-auth?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1'
+    )
+    .then(() => console.log('connected to mongodb'))
+    .catch((error) => console.log(error));
 
   app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-  })
-}
+    console.log(`server running on port ${PORT}`);
+  });
+};
 
-start()
+start();
