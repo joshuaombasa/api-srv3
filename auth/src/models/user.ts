4c8f54a7a298e7ab462484c09d0873b817d29f31
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-07 01:48:28 +0300

    initial commit

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -13,10 +13,9 @@ const userSchema = new mongoose.Schema({
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
-
     delete returnedObject._id;
-    delete returnedObject.__v;                                 
-    delete returnedObject.passowrd;
+    delete returnedObject.__v;
+    delete returnedObject.password;
   },
 });
 
